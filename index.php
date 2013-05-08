2cbba2fb9478be7337478c3a091ed017b30508da
Stefanos Markidis <stmarkidis@gmail.com>
2013-05-08 18:24:33 +0200

    Connect and retrieve

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+$dbconn = pg_connect("host=localhost port=5432 dbname=pg user=postgres password=bob12345");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,5 +9,17 @@
 </head>
 <body>
 <h1>Hello World!</h1>
+<?php  $rs = pg_query($dbconn,"SELECT * FROM users WHERE name='Stefanos'");
+echo '<table>';
+while($row = pg_fetch_assoc($rs))
+{
+	echo '<t>';
+	echo '<td align="center" width="50">'.$row['id'].'</td>';	
+	echo '<td align="center" width="50">'.$row['joned'].'</td>';
+	echo '<td align="center" width="50">'.$row['name'].'</td>';
+	echo '</tr>';	
+}
+echo '</table>';
+?>
 </body>
 </html>
