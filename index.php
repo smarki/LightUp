66ac2ef475c33b13c4c6081f40eaf06caa4b8cb9
Stefanos Markidis <stmarkidis@gmail.com>
2013-05-08 21:20:19 +0200

    Aplo INSERT kai SELECT
    Apli efarmogi pou psaxnei gia to onoma kai na den uparxei to vazei stin
    vasi dedomenwn

--- index.php
+++ index.php
@@ -9,7 +9,12 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=pg user=postgres password=
 </head>
 <body>
 <h1>Hello World!</h1>
-<?php  $rs = pg_query($dbconn,"SELECT * FROM users WHERE name='Stefanos'");
+<?php  
+$rs = pg_query($dbconn,"SELECT * FROM users WHERE name='Stefanos'");
+
+if(pg_num_rows($rs)==0){
+$rs = pg_query($dbconn,"INSERT INTO users (name) VALUES ('Stefanos')");	
+}
 echo '<table>';
 while($row = pg_fetch_assoc($rs))
 {
