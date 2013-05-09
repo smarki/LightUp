a14903fd2fa66ce811c0452e2cea523c1ff12ba9
Alex Koufa <akoufa@hotmail.com>
2013-05-09 22:46:00 +0300

    Dimiourgia index.php kai get_question.php gia tin epeksergasia tis erotisis

---
+++ get_question.php
@@ -0,0 +1,22 @@
+<?php
+
+
+session_start();
+
+// retrieve session data
+$dbconn = $_SESSION['dbconn'];
+
+//q_text contains the question text
+$q_text = $_POST['question'];
+//the name of the friend
+$f_name = $_POST['friend'];
+echo $q_text;
+
+// Prepare a query for execution
+$result = pg_prepare($dbconn, "f_query", 'SELECT * FROM users WHERE name = $1');
+
+
+
+// Execute the prepared query
+$result = pg_execute($dbconn, "f_query", $f_name);  //edo den eimai sigouros an epitrepetai to f_name
+?>
--- index.php
+++ index.php
@@ -1,30 +1,30 @@
-<?php 
-$dbconn = pg_connect("host=localhost port=5432 dbname=pg user=postgres password=bob12345");
+<?php
+
+session_start();
+
+$dbconn = pg_connect("host=localhost port=5432 dbname=pg user=postgres password=alex");
+// store session data
+$_SESSION['dbconn'] = $dbconn;
 ?>
+
 <!DOCTYPE html>
+
 <html>
 <head>
-<title>Gamiete o Dias</title>
-<script src="js/jquery.js"></script>
+<title>The Lighter ver 0.001</title>
+
 </head>
 <body>
-<h1>Hello World!</h1>
-<?php  
-$rs = pg_query($dbconn,"SELECT * FROM users WHERE name='Stefanos'");
+<p1>Welcome to the Lighter application</p1>
+<p2>Unfortunately for the moment you can't do a lot of interesting things</p2>
+<!--Parakato tha prepei na ginei elegxos simpliroseis apo tis formes me js-->
+<form name="input" action="get_question.php" method="post">
+Enter your question: 
+<input type="text" name="question"></br>
+Enter the name of a friend you want to pass the lighter to:
+<input type="text" name="friend"></br>
+<input type="submit" value="Submit your question"/></br>
+</form>
 
-if(pg_num_rows($rs)==0){
-$rs = pg_query($dbconn,"INSERT INTO users (name) VALUES ('Stefanos')");	
-}
-echo '<table>';
-while($row = pg_fetch_assoc($rs))
-{
-	echo '<t>';
-	echo '<td align="center" width="50">'.$row['id'].'</td>';	
-	echo '<td align="center" width="50">'.$row['joned'].'</td>';
-	echo '<td align="center" width="50">'.$row['name'].'</td>';
-	echo '</tr>';	
-}
-echo '</table>';
-?>
 </body>
 </html>
